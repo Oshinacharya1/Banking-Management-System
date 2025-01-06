1acefa50f129dfafdebf5b5ecb041c6d63cfa064
Oshin Acharya <oshin.acharya0664@gmail.com>
2025-01-06 15:48:07 -0600

    added loan and transection  entity

--- BankingManagement/src/main/java/com/project/BankingManagement/entity/Loan.java
+++ BankingManagement/src/main/java/com/project/BankingManagement/entity/Loan.java
@@ -1,4 +1,25 @@
 package com.project.BankingManagement.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
 public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type; // HOME, CAR, PERSONAL
+
+    private BigDecimal amount;
+
+    private Double interestRate;
+
+    private String status; // PENDING, APPROVED, REJECTED
+
+    @ManyToOne
+    private User user;
 }
