1022a33239ef8d9f56f592478e45e36ef2daba2c
Oshin Acharya <oshin.acharya0664@gmail.com>
2025-01-06 16:11:29 -0600

    created user repository

--- BankingManagement/src/main/java/com/project/BankingManagement/repository/UserRepository.java
+++ BankingManagement/src/main/java/com/project/BankingManagement/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.project.BankingManagement.repository;
 
-public interface UserRepository {
+import com.project.BankingManagement.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
