65b4e6dd36f62718516bd786261569cd8c7890b0
Oshin Acharya <oshin.acharya0664@gmail.com>
2025-01-06 16:21:00 -0600

    created account, loan, and transaction repository

--- BankingManagement/src/main/java/com/project/BankingManagement/repository/AccountRepository.java
+++ BankingManagement/src/main/java/com/project/BankingManagement/repository/AccountRepository.java
@@ -1,4 +1,12 @@
 package com.project.BankingManagement.repository;
 
-public interface AccountRepository {
+import com.project.BankingManagement.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByAccountNumber (String accountNumber);
 }
--- BankingManagement/src/main/java/com/project/BankingManagement/repository/LoanRepository.java
+++ BankingManagement/src/main/java/com/project/BankingManagement/repository/LoanRepository.java
@@ -1,4 +1,9 @@
 package com.project.BankingManagement.repository;
 
-public interface LoanRepository {
+import com.project.BankingManagement.entity.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
 }
