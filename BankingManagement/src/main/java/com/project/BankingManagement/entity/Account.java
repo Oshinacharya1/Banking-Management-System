ac526cbff121a097aefc24c8bbe0dc9253dbc1b2
Oshin Acharya <oshin.acharya0664@gmail.com>
2025-01-06 15:35:52 -0600

    added account entity

--- BankingManagement/src/main/java/com/project/BankingManagement/entity/Account.java
+++ BankingManagement/src/main/java/com/project/BankingManagement/entity/Account.java
@@ -1,4 +1,19 @@
 package com.project.BankingManagement.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String accountNumber;
+
+    private int balance;
+
+    @ManyToOne
+    private User user;
 }
